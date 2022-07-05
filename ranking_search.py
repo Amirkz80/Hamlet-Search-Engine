96d0d9c9c7df9526fde1ddc0c3c43c237f8ea0e8
Amir <amirkazemzadeh77@gmail.com>
2022-07-05 22:22:54 +0430

    dev: Add a seprate console environment for handeling ranked retrieval searches

---
+++ ranking_search.py
@@ -0,0 +1,44 @@
+"""
+In this script we perform booloean search
+"""
+
+from datetime import datetime
+from ranking_tokenizer import tokenize_query
+
+def main():
+
+    # Gets user input until user enters exit
+    while 1:
+
+        query = input("\nEnter the query, Enter 'exit' to exit : ")
+        # To monitor duration time
+        
+        t1 = datetime.now()
+        
+        # Exit the program
+        if query.lower() == 'exit':
+            return 1
+        
+        else:
+            # Cleans up the query, so we'll have raw strin
+            # query = tokenizer(query)
+            
+            # Repeat the program if string is empty or None
+            if (query.isspace() and query) or not query:
+                print('\nquery can not be empty!')
+            
+            # Search for keyword in file
+            else:
+                
+                # Tokenizing the query
+                words = tokenize_query(query)
+                print(words)
+
+        print(
+            f"searching index table time : ",
+            f"{(datetime.now() - t1).total_seconds()}"
+        )
+
+
+if __name__ == "__main__":
+    main()
