d4a427bf816a8230068cfc9459ea5f9d60a9939a
Amir <amirkazemzadeh77@gmail.com>
2022-07-06 19:34:07 +0430

    dev: Add utilities for idf weight to the ranker function and tf_idf_calculator

--- ranking_functions.py
+++ ranking_functions.py
@@ -1,4 +1,5 @@
 import math
+import os
 from index import tokenizer
 from boolean_operators import bool_search
 
@@ -17,11 +18,17 @@ def tokenize_query(query: str) -> list:
     return tokens
 
 
-def tf_idf_calculator(term_frequency: int) -> float:
+def tf_idf_calculator(raw_term_frequency: int, docs_with_token_length: int) -> float:
     """Calculates the TF-IDF score of the given document and returns it"""
 
-    tf = math.log10(1 + term_frequency)
-    return tf    
+    # Number of ALL docs in repo
+    N = len(os.listdir('repo/'))
+
+    tf = math.log10(1 + raw_term_frequency)
+    idf = math.log10(N/(docs_with_token_length + 1))
+
+    tf_idf = tf * idf
+    return tf_idf    
 
 
 def ranker(tokens: list) -> list:
@@ -36,18 +43,22 @@ def ranker(tokens: list) -> list:
     for token in tokens:
         # The dictionary that contains term frequnecy of the word
         token_info = bool_search(token)
+        # Number of all tokens that contain token
+        docs_with_token_num = len(token_info.keys())
 
+        # Iterate over all docs that contain the token
+        # They are in token term_frequncy dict (are the keys basically)
         for doc_id in token_info.keys():
             if doc_id not in doc_scores.keys():
-                doc_scores[doc_id] = tf_idf_calculator(token_info[doc_id])
+                doc_scores[doc_id] = tf_idf_calculator(token_info[doc_id], docs_with_token_num)
             else:
-                doc_scores[doc_id] += tf_idf_calculator(token_info[doc_id])
+                doc_scores[doc_id] += tf_idf_calculator(token_info[doc_id], docs_with_token_num)
     
     for pair in sorted(doc_scores.items(), key=lambda item: item[1]):
         final_docs.append(pair[0])
     
-    # We reverse the list to have ascending order
+    # We reverse the list to have descending order
     # This function modifies list directly
     final_docs.reverse()
-    
+
     return final_docs
