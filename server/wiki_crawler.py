b2b876a3ee5b99ea7914623ba1af2f3ace76a462
Amir <amirkazemzadeh77@gmail.com>
2022-08-09 18:57:54 +0430

    fix: Add check path Added check path to let crawler run for the first time Remember in the first run time there wouldn't been any urls.text yet

--- server/wiki_crawler.py
+++ server/wiki_crawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from os import path
 
 n = 1
 r = ''
@@ -42,35 +43,37 @@ def crawler(url: str):
     # Making request to the url
     r = requests.get(url)
 
-    if url != "https://en.wikipedia.org/wiki/Main_Page":
-        # Appending new file to the repo
-        with open(f'repo\{n}th file.txt', 'w', encoding="utf-8") as file:
-                file.write(url + "\n")
-                file.writelines([text + '\n' for text in parser(r).find_all(text=True) if text.parent.name not in blacklist and text != '\n'])
-        
-        n += 1
-        # Appending url to the crawled urls list
-        with open('index_table/urls.txt', 'a', encoding='utf-8') as file:
-            file.write(url + '\n')
-            file.write(str(n) + '\n')
+    # Appending new file to the repo
+    with open(f'repo\{n}th file.txt', 'w', encoding="utf-8") as file:
+            file.write(url + "\n")
+            file.writelines([text + '\n' for text in parser(r).find_all(text=True) if text.parent.name not in blacklist and text != '\n'])
+    
+    n += 1
+    # Appending url to the crawled urls list
+    with open('index_table/urls.txt', 'a', encoding='utf-8') as file:
+        file.write(url + '\n')
+        file.write(str(n) + '\n')
 
-        print(url)
+    print(url)
 
     for link in parser(r).find_all('a'):
         
         # Crawler cralws 10 pages in each call
         if called_num > 10:
             return 0
-        elif link.get("href") is not None and "/wiki/" == link.get("href")[:6] and check_url("https://en.wikipedia.org" + link.get("href")) :
+        elif link.get("href") is not None and "/wiki/" == link.get("href")[:6] and check_url("https://en.wikipedia.org" + link.get("href")) and ":" not in link.get("href"):
             crawler("https://en.wikipedia.org" + link.get("href"))
 
 def main():
     """Main function"""
     global n
-    with open('index_table/urls.txt', 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-        n = int(lines[-1])
-    print(f"n is equal to : {n}")
+
+    if path.exists("'index_table/urls.txt'"):
+        with open('index_table/urls.txt', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            n = int(lines[-1])
+        print(f"n is equal to : {n}")
+        
     crawler("https://en.wikipedia.org/wiki/Main_Page")
 
 if __name__ == '__main__':
