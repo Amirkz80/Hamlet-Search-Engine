ef55ae2834be3dc6f5c79ad71961a152a8bd598c
Amir <amirkazemzadeh77@gmail.com>
2022-08-09 17:37:39 +0430

    dev: Change summary and link content based on the new summary which is returned from back-end, And is equal to article link, I assigend the summary to link

--- client/components/Result.jsx
+++ client/components/Result.jsx
@@ -1,21 +1,21 @@
 import Link from "next/link"
 
 const Result = ({ title, summary }) => {
-  const link = `http://localhost:5000/files/${title}`
+    const link = summary /*`http://localhost:5000/files/${title}`*/
 
-  return (
-    <div className="flex flex-col p-4 mb-4 w-full rounded-lg hover:shadow-lg">
-      <Link href={link}>
-        <a
-          className="font-bold text-left text-blue-700 hover:underline"
-          target="blank"
-        >
-          {title}
-        </a>
-      </Link>
-      <p className="text-gray-600">{summary}</p>
-    </div>
-  )
+    return (
+        <div className="flex flex-col p-4 mb-4 w-full rounded-lg hover:shadow-lg">
+            <Link href={link}>
+                <a
+                    className="font-bold text-left text-blue-700 hover:underline"
+                    target="blank"
+                >
+                    {title}
+                </a>
+            </Link>
+            <p className="text-gray-600">{summary}</p>
+        </div>
+    )
 }
 
 export default Result
