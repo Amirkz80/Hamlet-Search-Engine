7ccc99de606eb748c4e56bd192adef01a3bb34ac
Amir <amirkazemzadeh77@gmail.com>
2022-08-11 19:15:55 +0430

    fix: Use heapq instead of my own sorting by using heap we increase the speed of sorting token_info and finding its 10 biggest members

--- server/ranking/ranking_functions.py
+++ server/ranking/ranking_functions.py
@@ -3,8 +3,7 @@ import os
 from datetime import datetime
 from index import tokenizer
 from boolean.boolean_operators import bool_search
-from multiprocessing import Pool
-from itertools import islice
+from heapq import nlargest
 
 def chunk(number: int, chunks_number: int):
     """Gets range of numbers and returns start and end number
@@ -38,41 +37,31 @@ def tokenize_query(query: str) -> list:
     
     return tokens
 
-
-def tf_idf_calculator(raw_term_frequency: int, docs_with_token_length: int) -> float:
+def tf_idf_calculator(raw_term_frequency: int, idf: float) -> float:
     """Calculates the TF-IDF score of the given document and returns it"""
 
-    return math.log10(1 + raw_term_frequency) * math.log10(len(os.listdir('repo/'))/(docs_with_token_length + 1))    
+    return math.log10(1 + raw_term_frequency) * idf    
 
-
-def ranker(doc_scores: dict, token_info: dict, length: int, start: int, end: int) -> list:
+def ranker(doc_scores: dict, length: int, token_info: dict) -> list:
     """ranks the docs that contain token and returns list of
     relative docs that are sorted by TF-IDF """
 
-    # doc_scores dictionary contains doc_ids and their TF-IDF score
 
+    idf = math.log10(len(os.listdir('repo/'))/(length + 1))
+    
+    # doc_scores dictionary contains doc_ids and their TF-IDF score
     # Iterate over all docs that contain the token
-    # They are in token term_frequncy dict (are the keys basically)
-    #i = 1
-    for doc_id in dict(islice(token_info.items(), start - 1, end)):
-        t1 = datetime.now()
-        
+    # They are in token term_frequncy dict (are the keys basically)    
+    for doc_id in token_info:
         if doc_id not in doc_scores:
-            doc_scores[doc_id] = tf_idf_calculator(token_info[doc_id], length)
-            #i += 1
-            #print(datetime.now() - t1)
+            doc_scores[doc_id] = tf_idf_calculator(token_info[doc_id], idf)
         else:
-            doc_scores[doc_id] += tf_idf_calculator(token_info[doc_id], length)
-        #print(datetime.now() - t1)
-        #i += 1
-    #print(i)
+            doc_scores[doc_id] += tf_idf_calculator(token_info[doc_id], idf)
 
     return doc_scores
 
 def rank_tokens(tokens: list) -> list:
-    """Gets tokens list, calls ranker for each of them
-    (call for each of them is done by Multiprocessing and 4 processes)
-    Returns the sorted list for all tokens"""
+    """Gets tokens list, calls ranker for each of them"""
     
     # final sorted by score docs
     final_docs = []
@@ -81,36 +70,29 @@ def rank_tokens(tokens: list) -> list:
     doc_scores = {}
 
     for token in tokens:
-
         # The dictionary that contains term frequnecy of the word
         token_info = bool_search(token)
         # Number of all documents that contain token
-        length = len(token_info.keys())
+        length = len(token_info)
 
-        groups = chunk(length, 4)
-        ranges = [
-            [doc_scores, token_info, length, groups[0][0], groups[0][1]],
-            [doc_scores, token_info, length, groups[1][0], groups[1][1]],
-            [doc_scores, token_info, length, groups[2][0], groups[2][1]],
-            [doc_scores, token_info, length, groups[3][0], groups[3][1]],
-        ]
+        # A dictionary to purify original token_info
+        final_dictionary = {}
+        # If dictionary is too large, we only rank its
+        # 10 members that have biggest values 
+        if length > 10:
+            for key in nlargest(10, token_info, key=token_info.get):
+                final_dictionary[key] = token_info[key]
 
-        with Pool(4) as p:
-            results = p.starmap(ranker, ranges)
-            #print(f"results : {results}")
-            for d in results:
-                for key, value in d.items():
-                    if key not in doc_scores:
-                        doc_scores[key] = value
-                    if key in doc_scores and value > doc_scores[key]:
-                        doc_scores[key] = value 
-
+            ranker(doc_scores=doc_scores, length=length, token_info=final_dictionary)
+        else:
+            ranker(doc_scores=doc_scores, length=length, token_info=token_info)
+        
     for pair in sorted(doc_scores.items(), key=lambda item: item[1]):
         final_docs.append(pair[0])
     
-    # reverse returns None!
-    # therefore we SHOULD NOT assign it to a var
-    final_docs.reverse()
+    # Reverse returns None!
+    # Therefore we SHOULD NOT assign it to a var
     # We reverse the list to have descending order
     # This function modifies list directly
+    final_docs.reverse()
     return final_docs
